2cbcf0645f86133007e97cadac7433fda0954326
Rustam Nuriddinov <rustamm.nuriddinov@gmai.com>
2024-03-06 22:05:57 +0500

    Xatoliklar uchun error qo'shildi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // env ni chaqiramiz
 require("dotenv").config();
-const { Bot } = require("grammy");
+const { Bot, GrammyError, HttpError } = require("grammy");
 
 // bot tokenini, env fayldan import qilamiz
 const bot = new Bot(process.env.BOT_API_TOKEN);
@@ -14,5 +14,21 @@ bot.command("start", async (ctx) => {
 bot.hears('HTML', async (ctx) => {
     await ctx.reply("HTML o'zi nima!")
 })
+
+
+// Error
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
 
 bot.start();
